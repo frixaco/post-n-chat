2a70cdb9ec496492da0456f4aa29f47a6537671b
frixaco <rustam21ashurmatov@gmail.com>
2020-07-13 21:31:22 +0500

    added github link

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -12,6 +12,11 @@ function Home() {
                 <Posts />
                 <Chat />
             </div>
+            <footer className='bg-light text-center py-2'>
+                <a rel="noopener noreferrer" target='_blank' className='github' href="https://github.com/frixaco/portfolio-project-1-social">
+                    <i class="fab fa-github fa-3x"></i>
+                </a>
+            </footer>
         </div >
     )
 }
