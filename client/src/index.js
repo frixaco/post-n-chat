d1283de4bcf623d8f90ccddaf4cb77e3a1a491d0
frixaco <rustam21ashurmatov@gmail.com>
2020-07-15 10:20:47 +0500

    returned strictmode; class to className

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,25 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-// import App from './App';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 
-import * as serviceWorker from './serviceWorker';
-import Spinner from './components/Spinner/Spinner';
+import Spinner from "./components/Spinner/Spinner";
 
-const App = React.lazy(() => import('./App'))
+const App = React.lazy(() => import("./App"));
 ReactDOM.render(
-  // <React.StrictMode>
-  <Suspense fallback={<Spinner />}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
-  </Suspense>,
-  // </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <Suspense fallback={<Spinner />}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+        </BrowserRouter>
+      </Provider>
+    </Suspense>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import Chat from '../../components/Chat/Chat';
-import Posts from '../../components/Posts/Posts';
-import HomeNavBar from '../../components/Navbars/HomeNavBar';
+import Chat from "../../components/Chat/Chat";
+import Posts from "../../components/Posts/Posts";
+import HomeNavBar from "../../components/Navbars/HomeNavBar";
 
 function Home() {
-    return (
-        <div className='home-container'>
-            <HomeNavBar />
-            <div className='posts-chat-container'>
-                <Posts />
-                <Chat />
-            </div>
-            <footer className='bg-light text-center py-2'>
-                <a rel="noopener noreferrer" target='_blank' className='github' href="https://github.com/frixaco/portfolio-project-1-social">
-                    <i class="fab fa-github fa-3x"></i>
-                </a>
-            </footer>
-        </div >
-    )
+  return (
+    <div className="home-container">
+      <HomeNavBar />
+      <div className="posts-chat-container">
+        <Posts />
+        <Chat />
+      </div>
+      <footer className="bg-light text-center py-2">
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          className="github"
+          href="https://github.com/frixaco/portfolio-project-1-social"
+        >
+          <i className="fab fa-github fa-3x"></i>
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
